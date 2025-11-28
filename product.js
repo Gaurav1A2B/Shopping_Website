de82a285560d1ef7f963a8f9bb2bf9b1517e21e1
Gaurav Joshi <150048308+Gaurav1A2B@users.noreply.github.com>
2025-11-28 09:24:20 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "Smartphone", price: 14999, category: "electronics", img: "https://via.placeholder.com/200" },
+  { id: 2, name: "Laptop", price: 55999, category: "electronics", img: "https://via.placeholder.com/200" },
+  { id: 3, name: "T-Shirt", price: 599, category: "clothes", img: "https://via.placeholder.com/200" },
+  { id: 4, name: "Jeans", price: 1299, category: "clothes", img: "https://via.placeholder.com/200" },
+  { id: 5, name: "Running Shoes", price: 2499, category: "shoes", img: "https://via.placeholder.com/200" }
+];
